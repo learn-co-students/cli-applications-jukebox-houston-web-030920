dc96aa2970bfea032f1e3036c0365764ef84a400
Rooted1 <ruthobey@gmail.com>
2020-03-10 13:54:51 -0500

    add run method and songs array

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,15 @@
+songs = [
+  "Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"
+]
+
 #helps users understand how to use jukebox
 def help
 
@@ -21,5 +33,10 @@ end
 
 #exits jukebox when users want to stop listening
 def exit_jukebox
+
+end
+
+#implement all helper methods to run jukebox
+def run
 
 end
