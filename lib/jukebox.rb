e789c8005d0827530c62d7ba1f0351dd54526c22
Rooted1 <ruthobey@gmail.com>
2020-03-10 14:08:43 -0500

    include help instructions in help method

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,7 +12,11 @@ songs = [
 
 #helps users understand how to use jukebox
 def help
-
+ puts "I accept the following commands:"
+ puts "- help : displays this help message"
+ puts "- list : displays a list of songs you can play"
+ puts "- play : lets you choose a song to play"
+ puts "- exit : exits this program"
 end
 
 #-------------------------
