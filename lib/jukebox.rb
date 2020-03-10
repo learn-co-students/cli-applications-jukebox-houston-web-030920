2e5e04b55fdd9bb60c0355d2d7090a4ae0a096a6
Rooted1 <ruthobey@gmail.com>
2020-03-10 13:44:15 -0500

    define helper methods

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,25 @@
-# Add your code here
+#helps users understand how to use jukebox
+def help
+
+end
+
+#-------------------------
+
+#outputs a list of songs users can select from
+def list
+
+end
+
+#-------------------------
+
+#plays songs users select by song name or track number
+def play
+
+end
+
+#-------------------------
+
+#exits jukebox when users want to stop listening
+def exit_jukebox
+
+end
